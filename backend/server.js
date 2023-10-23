3c3e83301c367236056b0764e37a6a63fcb39d21
soojin <soojinchoi323@gmail.com>
2023-10-23 02:12:25 -0700

    connect mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
-
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.use(express.json());
+app.use(cors());
+
 //middleware
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next()
-})
-//routes
-app.get('/', (req, res) =>{
-    res.json({msg : 'Welcome to real-time chat app'});
-})
+  console.log(req.path, req.method);
+  next();
+});
+
 //listen for requests
-app.listen(port, () =>{
-    console.log(`listening on port : ${port}`);
-})
+app.listen(port, () => {
+  console.log(`listening on port : ${port}`);
+});
+//cconnect to DB
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection succeed"))
+  .catch((error) => console.log("MongoDB connection failed:", error.message));
+//DB CRUD
+//crate
+// app.post();
+//read
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to real-time chat app" });
+});
+//upodate
+// app.put();
+//delete
+// app.delete();
