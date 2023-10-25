af967583135cf2a06797e04ef845d8283582e5f5
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-10-25 14:36:00 -0700

    improved design, added demo friend for testing

---
+++ frontend/src/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+
+    // Add other global styles as needed
+`;
+
+export default GlobalStyle;
