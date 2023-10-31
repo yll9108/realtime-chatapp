a8fa278dd54d0bad52676194fde2ae7c534ad98e
YenLun <yenlunliu@gmail.com>
2023-10-31 16:20:49 -0700

    connect login page frontend to backend done

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,23 +1,62 @@
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function SignUp() {
+    const [userName, setUserName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8080/api/users/register", {
+                userName,
+                email,
+                password,
+            })
+            .then((result) => {
+                console.log(result);
+                navigate("/");
+            })
+            .catch((err) => console.log(err));
+    };
     return (
         <>
             <SignUpDiv>
                 <h1>Sign up</h1>
-                <input
-                    type="userName"
-                    name="userName"
-                    placeholder="type in userName"
-                />
-                <input type="email" name="email" placeholder="type in email" />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="type in password"
-                />
-
-                <button>Sign Up</button>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <strong>Username</strong>
+                    </label>
+                    <input
+                        type="text"
+                        name="userName"
+                        placeholder="Enter userName"
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <label>
+                        <strong>Email</strong>
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="type in email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label>
+                        <strong>Password</strong>
+                    </label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="type in password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button>Sign Up</button>
+                </form>
             </SignUpDiv>
         </>
     );
