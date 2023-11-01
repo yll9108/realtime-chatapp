823bead0cd4e3f403a1e6c9c4d832491efa02e94
YenLun <yenlunliu@gmail.com>
2023-10-31 17:20:20 -0700

    connect FORGET PW page frontend to backend done

--- frontend/src/components/ForgettingPW.js
+++ frontend/src/components/ForgettingPW.js
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function ForgettingPW() {
+    const [email, setEmail] = useState();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8080/api/users/reset", {
+                email,
+            })
+            .then((result) => {
+                console.log(result);
+                navigate("/");
+            })
+            .catch((err) => console.log(err));
+    };
+
     return (
         <>
             <ForgettingPWDiv>
                 <h1>Forgot Password?</h1>
-                <input type="email" name="email" placeholder="type in email" />
-                <button>Sumbit</button>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <strong>Email</strong>
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="type in email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <button>Sumbit</button>
+                </form>
             </ForgettingPWDiv>
         </>
     );
