ea1041d6f856cab2e8ce9d2ac6662d87c74ad0b6
soojin <soojinchoi323@gmail.com>
2023-11-02 10:45:19 -0700

    organize Folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,60 @@
-
 import React, { useState, useEffect } from "react";
-import Login from "./components/Login";
+import Login from "./components/users/Login";
 import Chat from "./components/Chat";
 import SignUp from "./components/SignUp";
 import ForgettingPW from "./components/ForgettingPW";
 import ResetPW from "./components/ResetPW";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { auth } from './fireBaseConfig';
+import { auth } from "./fireBaseConfig";
 
 function App() {
-    // State to track login status
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // State to track login status
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    // Handler for logging in
-    const handleLogin = () => {
-        setIsLoggedIn(true);
-    };
+  // Handler for logging in
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
-    // Using useEffect to log updated state
-    useEffect(() => {
-        console.log("Updated isLoggedIn value:", isLoggedIn);
-        auth.onAuthStateChanged(user => {
-            if (user) {
-                handleLogin();
-            } else {
-                handleLogout();
+  // Using useEffect to log updated state
+  useEffect(() => {
+    console.log("Updated isLoggedIn value:", isLoggedIn);
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        handleLogin();
+      } else {
+        handleLogout();
+      }
+    });
+  }, [isLoggedIn]);
+
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      setIsLoggedIn(false);
+    });
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? (
+                <Chat handleLogout={handleLogout} />
+              ) : (
+                <Login handleLogin={handleLogin} />
+              )
             }
-        });
-    }, [isLoggedIn]);
-
-    const handleLogout = () => {
-        auth.signOut().then(() => {
-            setIsLoggedIn(false);
-        });
-      };
-      
-
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            isLoggedIn ? (
-                                <Chat handleLogout={handleLogout} />
-                            ) : (
-                                <Login handleLogin={handleLogin} />
-                            )
-                        }
-                    />
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgettingPW />}
-                    ></Route>
-                    <Route path="/signup" element={<SignUp />}></Route>
-                    <Route
-                        path="/reset/:resetToken"
-                        element={<ResetPW />}
-                    ></Route>
-                </Routes>
-            </div>
-        </Router>
-    );
+          />
+          <Route path="/forgot-password" element={<ForgettingPW />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/reset/:resetToken" element={<ResetPW />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
