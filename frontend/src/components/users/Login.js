7ef394132dd1a4efe92eb851f713a93ce2bf99f2
soojin <soojinchoi323@gmail.com>
2023-11-03 19:26:45 -0700

    Update Login.js
    moved handleSubmit fn to AuthContext

--- frontend/src/components/users/Login.js
+++ frontend/src/components/users/Login.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import { auth, googleAuthProvider } from "../../fireBaseConfig";
+import { AuthContext } from "../../context/AuthContext";
 
 function Login({ handleLogin }) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  // const [email, setEmail] = useState();
+  // const [password, setPassword] = useState();
 
   const signInWithGoogle = () => {
     auth
@@ -20,49 +21,60 @@ function Login({ handleLogin }) {
       });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post("http://localhost:8080/api/users/login", {
-        email,
-        password,
-      })
-      .then((result) => {
-        console.log("Logged in successfully", result);
-        // navigate("/");
-      })
-      .catch((err) => console.log(err));
-  };
-
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   axios
+  //     .post("http://localhost:8080/api/users/login", {
+  //       email,
+  //       password,
+  //     })
+  //     .then((result) => {
+  //       console.log("Logged in successfully", result);
+  //       // navigate("/");
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+  const { loginUser, updateLoginInfo, loginInfo, loginError, isLoginLoading } =
+    useContext(AuthContext);
   return (
     <LoginDiv>
       <h1>LOGIN</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <strong>Email</strong>
-        </label>
-        <input
-          type="email"
-          name="email"
-          placeholder="type in email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <label>
-          <strong>Password</strong>
-        </label>
-        <input
-          type="password"
-          name="password"
-          placeholder="type in password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button onClick={handleLogin}>Login</button>
+      <form onSubmit={loginUser}>
+        <Stack>
+          <label>
+            <strong>Email</strong>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="type in email"
+            onChange={(e) =>
+              updateLoginInfo({ ...loginInfo, email: e.target.value })
+            }
+          />
+        </Stack>
+        <Stack>
+          <label>
+            <strong>Password</strong>
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="type in password"
+            onChange={(e) =>
+              updateLoginInfo({ ...loginInfo, password: e.target.value })
+            }
+          />
+        </Stack>
+        <button type="submit">
+          {isLoginLoading ? "Login processing.." : "Login"}
+        </button>
       </form>
       <button onClick={signInWithGoogle}>Login/Signup with Google</button>
+      <Alert>
+        <p>{loginError}</p>
+      </Alert>
+
       <p>Forgot password?</p>
       <Link to="/forgot-password">Reset</Link>
       <h3>Don't have an account?</h3>
@@ -76,5 +88,12 @@ const LoginDiv = styled.div`
   flex-direction: column;
   align-items: center;
 `;
-
+const Stack = styled.div`
+  display: flex;
+`;
+const Alert = styled.div`
+  background-color: blue;
+  display: flex;
+  color: black;
+`;
 export default Login;
