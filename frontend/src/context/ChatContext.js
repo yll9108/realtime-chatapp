05b8fe2dbdb125be0e3b26198de00f419bb77043
soojin <soojinchoi323@gmail.com>
2023-11-06 10:23:36 -0800

    update chatcontext.js

---
+++ frontend/src/context/ChatContext.js
@@ -0,0 +1,89 @@
+import { createContext, useState, useEffect, useCallback } from "react";
+import { baseUrl, getRequest, postRequest } from "../utils/services";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children, user }) => {
+  const [userChats, setUserChats] = useState(null);
+  const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
+  const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseUrl}/users/`);
+      if (response.error) {
+        return console.log("ChatContextProvider getUsers error ", response);
+      }
+
+      const pChats = response.filter((u) => {
+        let isChatCreated = false;
+
+        if (user?._id === u._id) return false;
+
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return (
+              chat.roomMembers[0] === u._id || chat.roomMembers[1] === u._id
+            );
+          });
+        }
+
+        return !isChatCreated;
+      });
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
+
+  useEffect(() => {
+    const getUserChats = async () => {
+      if (user?._id) {
+        setIsUserChatsLoading(true);
+        setUserChatsError(null);
+
+        const response = await getRequest(`${baseUrl}/chats/${user?._id}`);
+
+        setIsUserChatsLoading(false);
+
+        if (response.error) {
+          return setUserChatsError(response);
+        }
+
+        setUserChats(response);
+      }
+    };
+    getUserChats();
+  }, [user]);
+
+  const createChat = useCallback(async (firstId, secondId) => {
+    const response = await postRequest(
+      `${baseUrl}/chats`,
+      JSON.stringify({
+        firstId,
+        secondId,
+      })
+    );
+
+    if (response.error) {
+      return console.log("createChat error", response);
+    }
+
+    setUserChats((prev) => [...prev, response]);
+  }, []);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        userChats,
+        isUserChatsLoading,
+        userChatsError,
+        potentialChats,
+        createChat,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
