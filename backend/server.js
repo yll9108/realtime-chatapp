6a6f8106e1890f5c58664273d1a70b29f335f767
soojin <soojinchoi323@gmail.com>
2023-11-06 11:24:46 -0800

    Update server.js
    comment google-login

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,9 @@ const { dbConnect } = require("./database.js");
 const userRoute = require("./routes/userRoute.js");
 const chatRoute = require("./routes/chatRoute.js");
 const messageRoute = require("./routes/messageRoute.js");
-const User = require('./models/userModel'); // Make sure to import your User model
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -21,13 +20,11 @@ app.use("/api/chats", chatRoute);
 // Message routes
 app.use("/api/messages", messageRoute);
 
-
-app.post('/api/users/google-login', userController.googleLogin);
-  
+// app.post("/api/users/google-login", userController.googleLogin);
 
 // Listen for requests
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
 
 // Connect to DB
@@ -35,5 +32,5 @@ dbConnect();
 
 // DB read
 app.get("/", (req, res) => {
-    res.json({ msg: "Welcome to real-time chat app" });
+  res.json({ msg: "Welcome to real-time chat app" });
 });
