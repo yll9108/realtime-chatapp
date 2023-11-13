d412e5afd8595c1896adb243712c33b450465fa3
soojin <soojinchoi323@gmail.com>
2023-11-12 22:10:57 -0800

    Update Chat.js

--- frontend/src/components/chats/Chat.js
+++ frontend/src/components/chats/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import ChatArea from "../chats/ChatArea";
 import UserList from "../chats/UserList";
 import Sidebar from "../shared/Sidebar";
@@ -10,74 +10,61 @@ import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
 import PotentialChats from "../chats/PotentialChats";
 
-const ChatContainer = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100vh;
-`;
-
 function Chat() {
-    // const demoChats = {
-    //     'John': {
-    //         messages: [
-    //             { type: "sent", content: "Hello how are you" },
-    //             { type: "received", content: "I'm good, thanks." }
-    //         ],
-    //         lastMessage: "I'm good, thanks.",
-    //         date: "2023-10-04"
-    //     },
-    //     'Doe': {
-    //         messages: [
-    //             { type: "sent", content: "Hey!" }
-    //         ],
-    //         lastMessage: "Hey!",
-    //         date: "2023-10-05"
-    //     }
-    // };
+  const { user } = useContext(AuthContext);
+  const { userChats, isUserChatsLoading, updateCurrentChat } =
+    useContext(ChatContext);
 
-    // const [selectedChat, setSelectedChat] = useState('John'); // default to John
-    const [activeSection, setActiveSection] = useState("chat"); // default to chat
-    const { user } = useContext(AuthContext);
-    const { userChats, isUserChatsLoading } = useContext(ChatContext);
+  return (
+    <ChatContainer>
+      <Sidebar />
+      <PotentialChats />
+      {userChats?.length < 1 ? null : (
+        <ChatList>
+          <Stack className="messages-box">
+            {isUserChatsLoading && <p>Loading Chats..</p>}
+            {userChats?.map((chat, index) => {
+              return (
+                <div key={index} onClick={() => updateCurrentChat(chat)}>
+                  <UserList chat={chat} user={user} />
+                </div>
+              );
+            })}
+          </Stack>
+          <ChatArea />
+        </ChatList>
+      )}
+    </ChatContainer>
 
-    return (
-        <ChatContainer>
-            <Sidebar setActiveSection={setActiveSection} />
-            <PotentialChats />
-            {userChats?.length < 1 ? null : (
-                <Stack className="chat-list">
-                    <Stack className="messages-box">
-                        {isUserChatsLoading && <p>Loading Chats..</p>}
-                        {userChats?.map((chat, index) => (
-                            <div key={index}>
-                                <UserList chat={chat} user={user} />
-                            </div>
-                        ))}
-                    </Stack>
-                    <ChatArea />
-                </Stack>
-            )}
-        </ChatContainer>
+    // <ChatContainer>
+    //  <Sidebar setActiveSection={setActiveSection} handleLogout={handleLogout} />
 
-        // <ChatContainer>
-        //  <Sidebar setActiveSection={setActiveSection} handleLogout={handleLogout} />
-
-        //     {activeSection === 'chat' && (
-        //         <>
-        //             <UserList setSelectedChat={setSelectedChat} chats={demoChats} />
-        //             <ChatArea chat={selectedChat} messages={demoChats[selectedChat].messages} chatName={selectedChat} onLogout={handleLogout} />
-        //         </>
-        //     )}
-        //     {activeSection === 'friends' && <Friends friendsData={demoChats} />}
-        //     {activeSection === 'settings' && <Settings />}
-        //     {activeSection === 'profile' && <Profile />}
-        // </ChatContainer>
-    );
+    //     {activeSection === 'chat' && (
+    //         <>
+    //             <UserList setSelectedChat={setSelectedChat} chats={demoChats} />
+    //             <ChatArea chat={selectedChat} messages={demoChats[selectedChat].messages} chatName={selectedChat} onLogout={handleLogout} />
+    //         </>
+    //     )}
+    //     {activeSection === 'friends' && <Friends friendsData={demoChats} />}
+    //     {activeSection === 'settings' && <Settings />}
+    //     {activeSection === 'profile' && <Profile />}
+    // </ChatContainer>
+  );
 }
+const ChatContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
 const Stack = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    gap: 4em;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 4em;
+`;
+const ChatList = styled.div`
+  display: flex;
+  align-items: start;
+  gap: 4em;
 `;
 export default Chat;
