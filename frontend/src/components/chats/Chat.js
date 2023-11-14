8eb140ec40efde459af9eea4dfa8bae5e66e83f0
soojin <soojinchoi323@gmail.com>
2023-11-14 12:51:59 -0800

    fix error
    shows username and send messages part

--- frontend/src/components/chats/Chat.js
+++ frontend/src/components/chats/Chat.js
@@ -1,11 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import ChatArea from "../chats/ChatArea";
 import UserList from "../chats/UserList";
 import Sidebar from "../shared/Sidebar";
 import styled from "styled-components";
-import Friends from "../settings/Friends";
-import Settings from '../settings/Settings';
-import Profile from '../settings/Profile';
+// import Friends from "../settings/Friends";
+// import Settings from './Settings';
+// import Profile from './Profile';
 import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
 import PotentialChats from "../chats/PotentialChats";
@@ -14,13 +14,10 @@ function Chat() {
   const { user } = useContext(AuthContext);
   const { userChats, isUserChatsLoading, updateCurrentChat } =
     useContext(ChatContext);
-    const [activeSection, setActiveSection] = useState('chat');
+
   return (
     <ChatContainer>
-      <Sidebar setActiveSection={setActiveSection} />
-            {activeSection === 'friends' && <PotentialChats/>}
-            {activeSection === 'settings' && <Settings />}
-            {activeSection === 'profile' && <Profile />}
+      <Sidebar />
       <PotentialChats />
       {userChats?.length < 1 ? null : (
         <ChatList>
@@ -71,4 +68,3 @@ const ChatList = styled.div`
   gap: 4em;
 `;
 export default Chat;
-
