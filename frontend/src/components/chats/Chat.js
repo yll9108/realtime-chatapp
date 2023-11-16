f1c8022603834b665f961da165ad6681fe60cba9
YenLun <yenlunliu@gmail.com>
2023-11-16 13:01:15 -0800

    chat error fixed

--- frontend/src/components/chats/Chat.js
+++ frontend/src/components/chats/Chat.js
@@ -1,62 +1,66 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import ChatArea from "../chats/ChatArea";
 import UserList from "../chats/UserList";
 import Sidebar from "../shared/Sidebar";
 import styled from "styled-components";
 // import Friends from "../settings/Friends";
-// import Settings from './Settings';
-// import Profile from './Profile';
+import Settings from "../settings/Settings";
+import Profile from "../settings/Profile";
 import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
 import PotentialChats from "../chats/PotentialChats";
-
 function Chat() {
-  const { user } = useContext(AuthContext);
-  const { userChats, isUserChatsLoading, updateCurrentChat } =
-    useContext(ChatContext);
-
-  return (
-    <ChatContainer>
-
-      <Sidebar setActiveSection={setActiveSection} />
-      {activeSection === 'friends' && <PotentialChats />}
-      {activeSection === 'settings' && <Settings />}
-      {activeSection === 'profile' && <Profile userProfile={user} />} 
-      {activeSection === 'chat' && userChats?.length > 0 && (
-
-      <PotentialChats />
-      {userChats?.length < 1 ? null : (
-
-        <ChatList>
-          <Stack className="messages-box">
-            {isUserChatsLoading && <p>Loading Chats..</p>}
-            {userChats?.map((chat, index) => (
-              <div key={index} onClick={() => updateCurrentChat(chat)}>
-                <UserList chat={chat} user={user} />
-              </div>
-            ))}
-          </Stack>
-          <ChatArea />
-        </ChatList>
-      )}
-    </ChatContainer>
-
-  );
+    const { user } = useContext(AuthContext);
+    const { userChats, isUserChatsLoading, updateCurrentChat } =
+        useContext(ChatContext);
+    const [activeSection, setActiveSection] = useState("chat");
+    return (
+        <ChatContainer>
+            <Sidebar setActiveSection={setActiveSection} />
+            {activeSection === "friends" && <PotentialChats />}
+            {activeSection === "settings" && <Settings />}
+            {activeSection === "profile" && <Profile userProfile={user} />}
+            {activeSection === "chat" && userChats?.length > 0 && (
+                <>
+                    {userChats?.length < 1 ? null : (
+                        <ChatList>
+                            <Stack className="messages-box">
+                                {isUserChatsLoading && <p>Loading Chats..</p>}
+                                {userChats?.map((chat, index) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            onClick={() =>
+                                                updateCurrentChat(chat)
+                                            }
+                                        >
+                                            <UserList chat={chat} user={user} />
+                                        </div>
+                                    );
+                                })}
+                            </Stack>
+                            <ChatArea />
+                        </ChatList>
+                    )}
+                </>
+            )}
+        </ChatContainer>
+    );
 }
 const ChatContainer = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100vh;
+    display: flex;
+    width: 100%;
+    height: 100vh;
 `;
 const Stack = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  gap: 4em;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 4em;
 `;
 const ChatList = styled.div`
-  display: flex;
-  align-items: start;
-  gap: 4em;
+    display: flex;
+    align-items: start;
+    gap: 4em;
 `;
 export default Chat;
