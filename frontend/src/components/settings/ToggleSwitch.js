1e9c285053aef4ae163bf1df42ac1ea1396a71c2
YenLun <yenlunliu@gmail.com>
2023-11-16 10:48:00 -0800

    fix error not done

--- frontend/src/components/settings/ToggleSwitch.js
+++ frontend/src/components/settings/ToggleSwitch.js
@@ -4,34 +4,49 @@ import axios from "axios";
 
 export default function ToggleSwitch({ label, userId, status }) {
     // console.log("UserId in ToggleSwitch:", userId);
-    const [isChecked, setIsChecked] = useState();
+    console.warn(status);
+    const [isChecked, setIsChecked] = useState(status);
     // console.log("isChecked", isChecked);
     // console.log("status", status);
 
+    // useEffect(() => {
+    //     setIsChecked(status);
+    // }, [status]);
+
     useEffect(() => {
-        setIsChecked(status);
         console.log("icChecked", isChecked);
-    }, [status]);
+    }, [isChecked]);
 
     const handleToggle = (e) => {
         if (!userId) {
             console.error("UserId is undefined!");
             return;
+        } else {
+            if (e.target.checked) {
+                console.log("checked");
+                setIsChecked(true);
+            } else {
+                console.log("unchecked");
+                setIsChecked(false);
+            }
+            axios
+                .post(`http://localhost:8080/api/settings/${userId}`, {
+                    showProfile:
+                        label === "Profile" ? e.target.checked : undefined,
+                    showStatus:
+                        label === "Status" ? e.target.checked : undefined,
+                    showAbout: label === "About" ? e.target.checked : undefined,
+                })
+                .then((res) => {
+                    console.log("line 40", res);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         }
-        setIsChecked(!isChecked);
+        // setIsChecked(!isChecked);
+
         // console.log("isChecked", !isChecked);
-        axios
-            .post(`http://localhost:8080/api/settings/${userId}`, {
-                showProfile: label === "Profile" ? !isChecked : undefined,
-                showStatus: label === "Status" ? !isChecked : undefined,
-                showAbout: label === "About" ? !isChecked : undefined,
-            })
-            .then((res) => {
-                console.log("line 40", res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
     };
     return (
         <Form>
