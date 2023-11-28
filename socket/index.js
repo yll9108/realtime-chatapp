f0508ffc65aff840eac804eaaf7ad57d07d26764
YenLun <yenlunliu@gmail.com>
2023-11-28 13:25:15 -0800

    transfer image into string

--- socket/index.js
+++ socket/index.js
@@ -1,45 +1,49 @@
 const { Server } = require("socket.io");
 
 const io = new Server({
-  cors: "http://localhost:3000",
+    cors: "http://localhost:3000",
 });
 
 let onlineUsers = [];
 
 io.on("connection", (socket) => {
-  console.log("new connection", socket.id);
+    console.log("new connection", socket.id);
 
-  //listen to a connection
-  socket.on("addNewUser", (userId) => {
-    userId !== null &&
-      !onlineUsers.some((user) => user.userId === userId) &&
-      onlineUsers.push({ userId, socketId: socket.id });
+    //listen to a connection
+    socket.on("addNewUser", (userId) => {
+        userId !== null &&
+            !onlineUsers.some((user) => user.userId === userId) &&
+            onlineUsers.push({ userId, socketId: socket.id });
 
-    console.log("socket onlineUsers", onlineUsers);
+        console.log("socket onlineUsers", onlineUsers);
 
-    io.emit("getOnlineUsers", onlineUsers);
-  });
+        io.emit("getOnlineUsers", onlineUsers);
+    });
 
-  //add message
-  socket.on("sendMessage", (message) => {
-    const user = onlineUsers.find(
-      (user) => user.userId === message.recipientId
-    );
+    //add message
+    socket.on("sendMessage", (message) => {
+        const user = onlineUsers.find(
+            (user) => user.userId === message.recipientId
+        );
 
-    if (user) {
-      io.to(user.socketId).emit("getMessage", message);
-      io.to(user.socketId).emit("getNotification", {
-        senderId: message.senderId,
-        isRead: false,
-        date: new Date(),
-      });
-    }
-  });
-  socket.on("disconnect", () => {
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+        if (user) {
+            if (message.type === "text") {
+                io.to(user.socketId).emit("getMessage", message);
+            } else if (message.type === "image") {
+                io.to(user.socketId).emit("getImage", message);
+            }
+            io.to(user.socketId).emit("getNotification", {
+                senderId: message.senderId,
+                isRead: false,
+                date: new Date(),
+            });
+        }
+    });
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
 
-    io.emit("getOnlineUsers", onlineUsers);
-  });
+        io.emit("getOnlineUsers", onlineUsers);
+    });
 });
 
 io.listen(4000);
