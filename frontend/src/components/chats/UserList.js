91eab8f0212e9feaee56821e0311ce3929fed649
soojin <soojinchoi323@gmail.com>
2023-12-05 03:55:17 -0800

    add search chat

--- frontend/src/components/chats/UserList.js
+++ frontend/src/components/chats/UserList.js
@@ -7,10 +7,12 @@ import { unreadNotificationsFunc } from "../../utils/unreadNotifications";
 import { useFetchLatestMessage } from "../../hooks/useFetchLatestMessage";
 import moment from "moment";
 
-const UserList = ({ chat, user }) => {
-  const { recipientUser } = useFetchRecipientUser(chat, user);
+const UserList = ({ index, chat, user, query }) => {
+  const { recipientUser } = useFetchRecipientUser(chat, user, query);
   const { onlineUsers, notifications, markThisUserNotificationsAsRead } =
     useContext(ChatContext);
+  const { updateCurrentChat } = useContext(ChatContext);
+
   const { latestMessage } = useFetchLatestMessage(chat);
   const unreadNotifications = unreadNotificationsFunc(notifications);
   const thisUserNotifications = unreadNotifications?.filter(
@@ -23,7 +25,6 @@ const UserList = ({ chat, user }) => {
     recipientUser && recipientUser.profilePicture
       ? `http://localhost:8080/${recipientUser.profilePicture}`
       : "";
-  // console.log("Profile Picture URL:", profilePictureUrl);
 
   const truncateText = (text) => {
     let shortText = text.substring(0, 20);
@@ -33,57 +34,58 @@ const UserList = ({ chat, user }) => {
     }
     return shortText;
   };
-  // console.log("userlist lastestMessage", latestMessage);
-  return (
-    <Stack
-      direction="horizontal"
-      gap={3}
-      className="user-card align-items-center p-2 justify-content-between"
-      role="button"
-      onClick={() => {
-        if (thisUserNotifications?.length !== 0) {
-          markThisUserNotificationsAsRead(thisUserNotifications, notifications);
-        }
-      }}
-    >
-      <div className="d-flex">
-        <div className="me-2">
-          {profilePictureUrl ? (
-            <UserAvatar src={profilePictureUrl} />
-          ) : (
-            <UserAvatar /> // Default avatar when no picture URL is available
-          )}
-        </div>
-        <div className="text-content">
-          <div className="name">
-            {recipientUser && recipientUser.userName
-              ? recipientUser.userName
-              : "unknown"}
-          </div>
-          <div className="text">
-            {latestMessage?.content && (
-              <span>{truncateText(latestMessage?.content)}</span>
+  // console.log("recipientUser", recipientUser);
+  return recipientUser ? (
+    <div key={index} onClick={() => updateCurrentChat(chat)}>
+      <Stack
+        direction="horizontal"
+        gap={3}
+        className="user-card align-items-center p-2 justify-content-between"
+        role="button"
+        onClick={() => {
+          if (thisUserNotifications?.length !== 0) {
+            markThisUserNotificationsAsRead(
+              thisUserNotifications,
+              notifications
+            );
+          }
+        }}
+      >
+        <div className="d-flex">
+          <div className="me-2">
+            {profilePictureUrl ? (
+              <UserAvatar src={profilePictureUrl} />
+            ) : (
+              <UserAvatar /> // Default avatar when no picture URL is available
             )}
           </div>
+          <div className="text-content">
+            <div className="name">{recipientUser.userName}</div>
+            <div className="text">
+              {latestMessage?.content && (
+                <span>{truncateText(latestMessage?.content)}</span>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="d-flex flex-column align-items-end">
-        <div className="date">
-          {moment(latestMessage?.updatedAt).calendar()}
+        <div className="d-flex flex-column align-items-end">
+          <div className="date">
+            {moment(latestMessage?.updatedAt).calendar()}
+          </div>
+          <div
+            className={
+              thisUserNotifications?.length > 0 ? "this-user-notifications" : ""
+            }
+          >
+            {thisUserNotifications?.length > 0
+              ? thisUserNotifications?.length
+              : ""}
+          </div>
+          <span className={isOnline ? "user-online" : ""}></span>
         </div>
-        <div
-          className={
-            thisUserNotifications?.length > 0 ? "this-user-notifications" : ""
-          }
-        >
-          {thisUserNotifications?.length > 0
-            ? thisUserNotifications?.length
-            : ""}
-        </div>
-        <span className={isOnline ? "user-online" : ""}></span>
-      </div>
-    </Stack>
-  );
+      </Stack>
+    </div>
+  ) : null;
 };
 
 const UserAvatar = styled.div`
