97b38ca8d750589a19ea6ccfe0eb1ffb1e24b5ec
YenLun <yenlunliu@gmail.com>
2023-12-13 20:44:00 -0800

    delete logo and change font

--- frontend/src/components/settings/AccountSettings.js
+++ frontend/src/components/settings/AccountSettings.js
@@ -4,72 +4,73 @@ import PopUp from "./PopUp";
 import { Container, Button, Stack } from "react-bootstrap";
 
 function AccountSettings({ setShowAccountSettings }) {
-  const { user, deleteAccount } = useContext(AuthContext);
-  const [passwordPopUp, setPasswordPopUp] = useState(false);
-  const [showEditAndDeleteButton, setShowEditAndDeleteButton] = useState(true);
-  const [isGoogleUser, setIsGoogleUser] = useState(false);
+    const { user, deleteAccount } = useContext(AuthContext);
+    const [passwordPopUp, setPasswordPopUp] = useState(false);
+    const [showEditAndDeleteButton, setShowEditAndDeleteButton] =
+        useState(true);
+    const [isGoogleUser, setIsGoogleUser] = useState(false);
 
-  const backButtonStyle = {
-    background: "none",
-    color: "inherit",
-    border: "none",
-    padding: "0",
-    font: "inherit",
-    cursor: "pointer",
-    outline: "inherit",
-    display: "flex",
-    alignItems: "center",
-  };
-  useEffect(() => {
-    if (user) {
-      setIsGoogleUser(user.isGoogleAccount);
-    }
-  }, [user]);
-  return (
-    <Container>
-      <Stack gap={2} className="col-md-20 mx-auto align-items-center">
-        <button
-          style={backButtonStyle}
-          onClick={() => setShowAccountSettings(false)}
-        >
-          <i className="bi bi-arrow-left"></i> Back to Settings
-        </button>
-        {!user.isGoogleAccount && (
-          <Button
-            variant="primary"
-            onClick={() => {
-              setPasswordPopUp(true);
-              setShowEditAndDeleteButton(false);
-            }}
-            style={{
-              display: showEditAndDeleteButton ? "block" : "none",
-              // backgroundColor: "red",
-            }}
-          >
-            Edit Password
-          </Button>
-        )}
+    const backButtonStyle = {
+        background: "none",
+        color: "inherit",
+        border: "none",
+        padding: "0",
+        font: "inherit",
+        cursor: "pointer",
+        outline: "inherit",
+        display: "flex",
+        alignItems: "center",
+    };
+    useEffect(() => {
+        if (user) {
+            setIsGoogleUser(user.isGoogleAccount);
+        }
+    }, [user]);
+    return (
+        <Container>
+            <Stack gap={2} className="col-md-20 mx-auto align-items-center">
+                <button
+                    style={backButtonStyle}
+                    onClick={() => setShowAccountSettings(false)}
+                >
+                    <i className="bi bi-arrow-left"></i> Back to Settings
+                </button>
+                {!user.isGoogleAccount && (
+                    <Button
+                        variant="primary"
+                        onClick={() => {
+                            setPasswordPopUp(true);
+                            setShowEditAndDeleteButton(false);
+                        }}
+                        style={{
+                            display: showEditAndDeleteButton ? "block" : "none",
+                            // backgroundColor: "red",
+                        }}
+                    >
+                        Edit Password
+                    </Button>
+                )}
 
-        <Button
-          className="btn btn-danger"
-          onClick={() => deleteAccount(user._id)}
-          style={{
-            display: showEditAndDeleteButton ? "block" : "none",
-            // backgroundColor: "red",
-          }}
-        >
-          Delete Account
-        </Button>
-        {passwordPopUp && (
-          <PopUp
-            userId={user._id}
-            trigger={passwordPopUp}
-            setTrigger={setPasswordPopUp}
-          />
-        )}
-      </Stack>
-    </Container>
-  );
+                <Button
+                    className="btn btn-danger"
+                    onClick={() => deleteAccount(user._id)}
+                    style={{
+                        display: showEditAndDeleteButton ? "block" : "none",
+                        backgroundColor: "red",
+                    }}
+                >
+                    Delete Account
+                </Button>
+                {passwordPopUp && (
+                    <PopUp
+                        userId={user._id}
+                        trigger={passwordPopUp}
+                        setTrigger={setPasswordPopUp}
+                    />
+                )}
+            </Stack>
+        </Container>
+    );
 }
 
 export default AccountSettings;
--- frontend/src/globalStyles.js
+++ frontend/src/globalStyles.js
@@ -1,10 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        font-family: Arial, sans-serif;
+        font-family: 'Open Sans', sans-serif;
+        font-weight: 400;
     }
 
     // Add other global styles as needed
